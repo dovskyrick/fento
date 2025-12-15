3366cb46ab14efd01a5db1f1e265a62d14ebc613
Ricardo Santos <05737@protonmail.com>
2025-12-15 05:39:53 +0000

    building added, next step camera limits and door entries

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,43 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { useGLTF, OrbitControls } from '@react-three/drei'
 
-export default function App() {
+
+
+function Credits() {
   return (
-    <Canvas>
-      {/* light */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[3, 3, 3]} />
+    <div
+      style={{
+        position: 'fixed',
+        bottom: '12px',
+        right: '12px',
+        color: 'white',
+        fontSize: '12px',
+        opacity: 0.7,
+        fontFamily: 'monospace',
+        pointerEvents: 'none',
+      }}
+    >
+      "SKVOT Lesson / Low-Poly Futuristic Building" (https://skfb.ly/oZB6v) by jsandorblendercourse is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+    </div>
+  )
+}
 
-      {/* object */}
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+function Model() {
+  const { scene } = useGLTF('/building.glb')
+  return <primitive object={scene} scale={1} />
+}
 
-      {/* controls */}
-      <OrbitControls />
-    </Canvas>
+export default function Scene() {
+  return (
+    <>
+      <Canvas camera={{ position: [3, 3, 3], fov: 50 }}>
+        <ambientLight intensity={0.8} />
+        <directionalLight position={[5, 5, 5]} />
+        <Model />
+        <OrbitControls />
+      </Canvas>
+
+      <Credits />
+    </>
   )
 }
